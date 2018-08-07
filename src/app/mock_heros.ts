bc4fd8cee7089248ed15743ad43fc15ed1ffc6cc
Terry Chen <terry7737@gmail.com>
2018-08-07 10:32:32 -0700

    second commit

---
+++ src/app/mock_heros.ts
@@ -0,0 +1,28 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+  new Hero(21, "Terry")
+];
+
+export const HEROESJ =  {
+     "11":{"name": "Mr. Nice", "type": "Teacher", "phone":"5039982666"}, 
+     "12":{"name": "Narco", "type": "Student", "phone":"2133219999" }
+};
+
+export const HEROEST  = [
+    'Mr. Nice'
+    , 'Narco' 
+    , 'Bombasto'
+  ];
+
+
